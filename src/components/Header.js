7b78d5d694f4e762ae55a12fa3e11d281c7c52d7
Vivek Kumar Shrivastava <shrivastava.96vivek@gmail.com>
2021-05-02 01:00:51 +0530

    Updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,25 @@
-const Header = () => {
-  return <div>Task Manager</div>;
+import PropTypes from "prop-types";
+import Button from "./Button";
+
+const Header = ({ title }) => {
+  const onClickHandler = () => {
+    console.log("Click");
+  };
+
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button color="blue" text="Add" onClick={onClickHandler} />
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  title: "Task Manager",
+};
+
+Header.protoTypes = {
+  title: PropTypes.string.isRequired,
 };
 
 export default Header;
