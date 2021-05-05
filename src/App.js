a7bf0aa2a20a47cb85bad9e1804332b2cf8bf99c
Vivek Kumar Shrivastava <shrivastava.96vivek@gmail.com>
2021-05-05 23:57:28 +0530

    update

--- src/App.js
+++ src/App.js
@@ -20,6 +20,12 @@ function App() {
       day: "Feb 6th at 1:30pm",
       reminder: true,
     },
+    {
+      id: 3,
+      text: "Meeting with Fdiend",
+      day: "Sat 7th at 9:30pm",
+      reminder: true,
+    },
   ]);
 
   const addTask = (task) => {
